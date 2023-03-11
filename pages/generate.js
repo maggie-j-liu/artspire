6ca74c72ccf1339b600b0b588042b70a8a619c90
Maggie Liu <maggie.j.liu@gmail.com>
2023-03-11 10:42:58 -0800

    add some styling

--- pages/generate.js
+++ pages/generate.js
@@ -9,17 +9,41 @@ const Generate = () => {
     setIdea(ai.response);
   };
   return (
-    <div>
-      <h1>Generate ideas 💡</h1>
-      <button
-        className="bg-blue-400 py-1 px-2"
-        onClick={() => {
-          getGeneration();
-        }}
-      >
-        Generate
-      </button>
-      <p>idea: {idea}</p>
+    <div className="min-h-screen">
+      <div className="mx-auto max-w-3xl px-8 py-4">
+        <h1 className="text-center text-4xl font-semibold">
+          Generate ideas 💡
+        </h1>
+        <div className="flex w-full justify-center">
+          <button
+            className="rounded-md bg-fuchsia-500 py-1.5 px-8 text-xl font-semibold text-white shadow-md duration-150 hover:scale-105"
+            onClick={() => {
+              getGeneration();
+            }}
+          >
+            Generate
+          </button>
+        </div>
+        <p>idea: {idea}</p>
+
+        <div className="h-4" />
+        <hr />
+        <div className="h-4" />
+        <div>
+          <h2 className="text-xl">Supplies</h2>
+          <ul>
+            <li>crayons</li>
+            <li>pens</li>
+          </ul>
+        </div>
+        <div>
+          <h2 className="text-xl">Colors</h2>
+          <ul>
+            <li>blue</li>
+            <li>white</li>
+          </ul>
+        </div>
+      </div>
     </div>
   );
 };
