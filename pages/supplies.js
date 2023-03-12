582b47e7da1555921870c2934538ee2761f1a8e0
Yo Sohn <yosohn@Judys-MacBook-Air.local>
2023-03-11 17:05:18 -0800

    home page

--- pages/supplies.js
+++ pages/supplies.js
@@ -1,10 +1,10 @@
 import { useState } from "react"
 import Link  from "next/link"
+import { Alex_Brush } from "next/font/google"
 export default function Home() {
     const defaultSupplies = [
-        "pencil","crayon","paint","brush"
+        ["pencil","pencil.jpg"],["crayon","crayon.jpg"],["paint","paint.jpg"],["brush","brush.jpg"],["pen","pen.jpg"],["watercolor","watercolor.jpg"],["pastels", "pastels.jpg"],["clay","clay.jpg"]
     ]
-    
     const [supplies, setSupplies] = useState(() => defaultSupplies.map(sup => ({name: sup, chosen: false})))
     const [newSupplyName, setNewSupplyName] = useState("")
     function addsup(supply) {
@@ -21,6 +21,7 @@ export default function Home() {
         setSupplies([...supplies, { name: newSupplyName, chosen: true }])
         setNewSupplyName("")
     }
+    let num = 0;
 
     
     return (
@@ -31,18 +32,22 @@ export default function Home() {
                     <br></br>
                     <div className="grid gap-4 grid-cols-3">
                         {supplies.map((supply) => {
+                            num++
                             return (
-                                <button key={supply.name} type="button" className ={`rounded-lg px-4 py-4 border border-2 ${supply.chosen ? "border-emerald-300 bg-emerald-50 hover:border-emerald-400" : "bg-white border-gray-300 hover:border-gray-400"}`} onClick={() => {addsup(supply.name)}}>{supply.name}
-                                </button>
-                            )
-                        })}
-                        <div
-                            className ={`rounded-lg px-4 py-4 border border-2 bg-white border-gray-300 hover:border-gray-400`}
-                        >
+                                <>
+                                <button key={supply.name} type="button" className ={`rounded-lg px-4 py-4 border-2 ${supply.chosen ? "border-emerald-300 bg-emerald-50 hover:border-emerald-400" : "bg-white border-gray-300 hover:border-gray-400"}`} onClick={() => {addsup(supply.name)}}>{supply.name}
+                                <img class = "img2" src = {defaultSupplies[num-1][1]}></img> </button>
+                                </>
+                                )
+                            })}
+                            <div
+                            className ={`rounded-lg px-4 py-4 border-2 bg-white border-gray-300 hover:border-gray-400`}
+                            >
                             <input value={newSupplyName} onChange={(e) => setNewSupplyName(e.target.value)} type="text" className="border-b-4" />
                             <button type="button" className="addbut" onClick={() => createSupply()}> &nbsp;&nbsp;&nbsp;<b>	&#10133;</b></button>
                         </div>
                     </div>
+    
                 </div>
             </div>
             <center>
@@ -52,7 +57,7 @@ export default function Home() {
                     </Link>
                 </div>
             </center>
-            
         </div>
+
     );
 }
